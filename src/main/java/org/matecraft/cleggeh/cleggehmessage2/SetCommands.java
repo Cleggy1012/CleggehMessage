536e66d564a91f750dbec4a8272befde4f4b7690
Cleggy1012 <martin@martinclegg.co.uk>
2015-08-02 11:57:22 +0100

    Refactored. Less Code + More Tea == Happy Bunny.

--- src/main/java/org/matecraft/cleggeh/cleggehmessage2/SetCommands.java
+++ src/main/java/org/matecraft/cleggeh/cleggehmessage2/SetCommands.java
@@ -1,8 +1,5 @@
 package org.matecraft.cleggeh.cleggehmessage2;
 
-import org.bukkit.entity.Player;
-import org.bukkit.ChatColor;
-
 public class SetCommands {
     
 
@@ -10,48 +7,10 @@ public class SetCommands {
     public SetCommands(CleggehMessage plugin) {
         this.plugin = plugin;
     }
-    void setstaff(Player p, String[] args) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i < args.length; ++i) {
-            String appended = args[i] + " ";
-            str.append(appended);
-        }
-        String staff = str.toString();
-        plugin.getConfig().set("staffmessage", staff);
+    
+    void setMessage(String type, String message) {
+        plugin.getConfig().set(type + "message", message);
         plugin.saveConfig();
-        p.sendMessage(ChatColor.GREEN + "Staff message set to: " + ChatColor.WHITE + staff);
     }
-    void setmember(Player p, String[] args) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i < args.length; ++i) {
-            String appended = args[i] + " ";
-            str.append(appended);
-        }
-        String member = str.toString();
-        plugin.getConfig().set("membermessage", member);
-        plugin.saveConfig();
-        p.sendMessage(ChatColor.GREEN + "Member message set to: " + ChatColor.WHITE + member);
-    }
-        void setbuilder(Player p, String[] args) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i < args.length; ++i) {
-            String appended = args[i] + " ";
-            str.append(appended);
-        }
-        String builder = str.toString();
-        plugin.getConfig().set("buildermessage", builder);
-        plugin.saveConfig();
-        p.sendMessage(ChatColor.GREEN + "Builder message set to: " + ChatColor.WHITE + builder);
-    }
-        void setgladiator(Player p, String[] args) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 1; i < args.length; ++i) {
-            String appended = args[i] + " ";
-            str.append(appended);
-        }
-        String gladiator = str.toString();
-        plugin.getConfig().set("gladiatormessage", gladiator);
-        plugin.saveConfig();
-        p.sendMessage(ChatColor.GREEN + "Gladiator message set to: " + ChatColor.WHITE + gladiator);
-    }
+    
 }
