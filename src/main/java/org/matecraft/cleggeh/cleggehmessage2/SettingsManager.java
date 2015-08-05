e8734523a6542619fcf9aa4ff7e1476a94f2ffb1
cleggy1012 <martin@martinclegg.co.uk>
2015-08-05 13:08:15 +0100

    Made Data.yml and Config.yml CopyDefault

--- src/main/java/org/matecraft/cleggeh/cleggehmessage2/SettingsManager.java
+++ src/main/java/org/matecraft/cleggeh/cleggehmessage2/SettingsManager.java
@@ -7,104 +7,82 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.logging.Level;
-import org.bukkit.Bukkit;
-import static org.bukkit.Bukkit.getLogger;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 public class SettingsManager {
-        private final CleggehMessage plugin;
-        public SettingsManager(CleggehMessage plugin) {
+
+    private final CleggehMessage plugin;
+
+    public SettingsManager(CleggehMessage plugin) {
         this.plugin = plugin;
     }
-        FileConfiguration config;
-        File cfile;
-        
-        FileConfiguration data;
-        File dfile;
-        private boolean copyDefault(String source, String dest) {
-            File destFile = new File(plugin.getDataFolder(), dest);
-            if (!destFile.exists()) {
-                try {
-                    destFile.getParentFile().mkdirs();
-                    InputStream in = getClass().getClassLoader().getResourceAsStream(source);
-                    if (in != null) {
-                        try {
-                            try (OutputStream out = new FileOutputStream(destFile)) {
-                                ByteStreams.copy(in, out);
-                            }
-                        }
-                        finally {
-                            in.close();
-                        }
-                        return true;
-                    }
-                }
-                catch (IOException e) {
-                    getLogger().log(Level.WARNING, "Error copying default " + dest, e);
-                }
-            }
-            
-            
-            return false;
-        }
-        
-        public void setup(Plugin p) {
-            cfile = new File(p.getDataFolder(), "config.yml");
-                if (!cfile.exists()) {
-                    try {
-                        cfile.createNewFile();
-                    }
-                    catch (IOException e) {
-                        Bukkit.getServer().getLogger().severe(ChatColor.RED + "Could not create Config.yml");
-                    }
-                }
-            dfile = new File(p.getDataFolder(), "data.yml");
-                if (!dfile.exists()) {
-                    try {
-                        dfile.createNewFile();
-                    }
-                    catch (IOException e) {
-                        Bukkit.getServer().getLogger().severe(ChatColor.RED + "Could not create data.yml!");
-                    }
-                }
-                data = YamlConfiguration.loadConfiguration(dfile);
-        }
-        
-        public FileConfiguration getData() {
-            return data;
-        }
-        
-        public void saveData(){
-            try {
-                data.save(dfile);
-            }
-            catch (IOException  e) {
-                Bukkit.getServer().getLogger().severe(ChatColor.RED + "Could not save data.yml");
-            }
-        }
-        
-        public void reloadData() {
-            data = YamlConfiguration.loadConfiguration(dfile);
-        }
-        
-        public FileConfiguration getConfig() {
-            return config;
-        }
-        
-        public void saveConfig() {
-            try {
-                config.save(cfile);
-            }
-            catch (IOException e) {
-                Bukkit.getServer().getLogger().severe(ChatColor.RED + "Could not save config.yml");
-            }            
-        }
-        
-        public void reloadConfig(){
-            config = YamlConfiguration.loadConfiguration(cfile);
-        }
-}
+    FileConfiguration config;
+    File cfile;
 
+    FileConfiguration data;
+    File dfile;
+
+    private boolean copyDefault(String source, String dest) {
+        File destFile = new File(plugin.getDataFolder(), dest);
+        if (!destFile.exists()) {
+            try {
+                destFile.getParentFile().mkdirs();
+                InputStream in = getClass().getClassLoader().getResourceAsStream(source);
+                if (in != null) {
+                    try {
+                        try (OutputStream out = new FileOutputStream(destFile)) {
+                            ByteStreams.copy(in, out);
+                        }
+                    } finally {
+                        in.close();
+                    }
+                    return true;
+                }
+            } catch (IOException e) {
+                plugin.getLogger().log(Level.WARNING, "Error copying default " + dest, e);
+            }
+        }
+
+        return false;
+    }
+
+    public void setup(Plugin p) {
+        copyDefault("config.yml", "config.yml");
+        copyDefault("data.yml", "data.yml");
+    }
+
+    public FileConfiguration getData() {
+        return data;
+    }
+
+    public void saveData() {
+        try {
+            data.save(dfile);
+        } catch (IOException e) {
+            plugin.getLogger().severe(ChatColor.RED + "Could not save data.yml");
+        }
+    }
+
+    public void reloadData() {
+        data = YamlConfiguration.loadConfiguration(dfile);
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(cfile);
+        } catch (IOException e) {
+            plugin.getLogger().severe(ChatColor.RED + "Could not save config.yml");
+        }
+    }
+
+    public void reloadConfig() {
+        config = YamlConfiguration.loadConfiguration(cfile);
+    }
+}
